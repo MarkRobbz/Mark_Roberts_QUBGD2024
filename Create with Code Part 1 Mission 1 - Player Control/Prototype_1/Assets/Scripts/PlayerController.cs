96e29d3e6a3c05a6768ae84e8aac3d7c87cbfb88
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2024-03-24 22:40:29 +0000

    Tutorial 1.4 Complete-Plane Programming

--- Create with Code Part 1 Mission 1 - Player Control/Prototype_1/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype_1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-     public float speed = 5f;
+     private float speed = 5.0f;
+     private float turnSpeed = 25.0f;
+     private float horizontalInput;
+     private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Play Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //Turns the vechicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
