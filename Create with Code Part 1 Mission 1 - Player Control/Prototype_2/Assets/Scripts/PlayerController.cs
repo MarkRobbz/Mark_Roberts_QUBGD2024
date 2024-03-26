41deded6dce15c7357deb941f3d863fe93245bc3
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2024-03-26 23:11:50 +0000

    Tutorial 2.2 Complete

--- Create with Code Part 1 Mission 1 - Player Control/Prototype_2/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype_2/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float rangeOfX = 10f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
         if (transform.position.x < -rangeOfX)
         {
             transform.position = new Vector3(-rangeOfX, transform.position.y, transform.position.z);
@@ -24,7 +28,11 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(rangeOfX, transform.position.y, transform.position.z);
         }
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        
     }
 }
