a5a34d931951c5f94ec230db5e18644f21be263b
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2024-06-03 20:38:08 +0100

    Section 4.3 Complete

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype_5/Assets/Scripts/GameManager.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype_5/Assets/Scripts/GameManager.cs
@@ -4,27 +4,64 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI scoreText;
+    private int score;
+    public bool isGameActive;
+    public Button restartBttn;
     
 
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
+        score = 0;
+        updateScore(0);
+        
+
     }
-    
+
+   
+
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+           
         }
         
+    }
+
+    public void updateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.GameObject().SetActive(true);
+        restartBttn.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameOverText.GameObject().SetActive(false);
+        restartBttn.gameObject.SetActive(false);
     }
 }
--- Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype_5/Assets/Scripts/Targets.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype_5/Assets/Scripts/Targets.cs
@@ -5,29 +5,45 @@ using UnityEngine;
 public class Targets : MonoBehaviour
 {
     private Rigidbody targetRB;
+    private GameManager _gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+ 
 
     // Start is called before the first frame update
     void Start()
     {
         targetRB = gameObject.GetComponent<Rigidbody>();
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         
         targetRB.AddForce(RandomForce(), ForceMode.Impulse);
         targetRB.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+     
     }
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        if (_gameManager.isGameActive)
+        {
+            _gameManager.updateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            Destroy(gameObject);
+        }
+       
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("BadItem"))
+        {
+            _gameManager.GameOver();
+        }
     }
     Vector3 RandomForce()
     {
